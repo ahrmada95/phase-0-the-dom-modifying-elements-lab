336e44d8de0a6305405f9ad328d5a1a0a7d503be
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-06-20 17:32:41 -0400

    commit tree b394276c2b8a1db5ed242397b52ac7db8c147559

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main = document.getElementById("main");
+main.remove()
+
+//Why does this work but removing const does not?
+//What am I creating here exactly?
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Ahnaf is the champion";
